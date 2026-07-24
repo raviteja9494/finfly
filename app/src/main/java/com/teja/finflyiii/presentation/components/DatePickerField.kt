b74ce7574c1c0631102ebee62f06535f15f5aafe
Vadapalli Raviteja <raviteja.vadapalli94@gmail.com>
2026-07-24 21:59:34 +0530

    Fix date picker BoxScope import

--- app/src/main/java/com/teja/finflyiii/presentation/components/DatePickerField.kt
+++ app/src/main/java/com/teja/finflyiii/presentation/components/DatePickerField.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.CalendarMonth
